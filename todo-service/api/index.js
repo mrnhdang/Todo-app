12ecb2b57001b93774154be9eb1ae3934cdac1d5
CinemaProject <huudang23102001@gmail.com>
2024-06-05 01:43:17 +0700

    test

--- todo-service/api/index.js
+++ todo-service/api/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const Todo = require("../models/Todo.model.js");
 require('dotenv').config();
 const app = express();
-const port = 8080;
+const port = 3001;
 
 app.use(express.json()); 
 app.use(cors());
